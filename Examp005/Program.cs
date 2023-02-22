a31d31f8762f1d5b5cae909b4253b6f6616d09bb
Alexandr <ssanek1995@gmail.com>
2023-02-22 11:40:10 +0300

    Ex5+Commands

--- Examp005/Program.cs
+++ Examp005/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+Console.WriteLine("Введите имя пользователя:");
+string name = Console.ReadLine();
+
+if(name.ToLower()=="маша"){ Console.WriteLine("Ура!!! Привет МАША!"); }
+else {Console.WriteLine("Привет " + name);}
+
