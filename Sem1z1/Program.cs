0dedeb712829066bf1b1e10b4c61db93a79a9336
Alexandr <ssanek1995@gmail.com>
2023-02-25 15:09:28 +0300

    Отредактировали sem1z1

--- Sem1z1/Program.cs
+++ Sem1z1/Program.cs
@@ -14,32 +14,43 @@ else
 {
     Console.WriteLine($"x={X}, Y={Y} -> Нет"); ;
 }
+
+
+
 Console.Write("Введите НОМЕР дня недели: ");
 int z = int.Parse(Console.ReadLine() ?? "");
-while (z<1 || z>7) {
+while (z < 1 || z > 7)
+{
     Console.WriteLine("ОШИБКА!!! ВВЕДИТЕ ЧИСЛО ОТ 1 ДО 7 ");
     Console.Write("Введите номер дня недели: ");
     z = int.Parse(Console.ReadLine() ?? "");
 }
-if (z==1){
+if (z == 1)
+{
     Console.WriteLine($"{z} день -> ПОНЕДЕЛЬНИК");
 }
-if (z==2){
+if (z == 2)
+{
     Console.WriteLine($"{z} день -> ВТОРНИК");
 }
-if (z==3){
+if (z == 3)
+{
     Console.WriteLine($"{z} день -> СРЕДА");
 }
-if (z==4){
+if (z == 4)
+{
     Console.WriteLine($"{z} день -> ЧЕТВЕРГ");
 }
-if (z==5){
+if (z == 5)
+{
     Console.WriteLine($"{z} день -> ПЯТНИЦА");
 }
-if (z==6){
+if (z == 6)
+{
     Console.WriteLine($"{z} день -> СУББОТА");
 }
-if (z==7){
+if (z == 7)
+{
     Console.WriteLine($"{z} день -> ВОСКРЕСЕНЬЕ");
 }
 
