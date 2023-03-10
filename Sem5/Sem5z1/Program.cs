7a1cc81a1dcaa55f9ce4b750d031cfc0ef3c8585
Alexandr <ssanek1995@gmail.com>
2023-03-10 17:17:52 +0300

    Семинар 5+ДЗ+команды

---
+++ Sem5/Sem5z1/Program.cs
@@ -0,0 +1,40 @@
+﻿//31. Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+//Найдите сумму отрицательных и положительных элементов массива.
+Console.Clear();
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine(String.Join(" ", array));
+int positiveSum = GetPositiveSum(array);
+int negativeSum = GetNegativeSum(array);
+Console.WriteLine($"Positive sum = {positiveSum}, negative sum = {negativeSum} ");
+//////////////////////////////////////////////////////////////////////////////////
+// Возвращает массив из size элементов,
+// заполненный случайными числами из промежутка [minValue, maxValue]
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+// Возвращает сумму положительных чисел массива arr
+int GetPositiveSum(int[] arr)
+{
+    int positiveSum = 0;
+    foreach (int el in arr)
+    {
+        if (el > 0) positiveSum += el;
+    }
+    return positiveSum;
+}
+// Возвращает сумму отрицательных чисел массива arr
+int GetNegativeSum(int[] arr)
+{
+    int negativeSum = 0;
+    foreach (int el in arr)
+    {
+        negativeSum += el < 0 ? el : 0;
+    }
+    return negativeSum;
+}
