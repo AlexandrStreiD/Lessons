5eacab7ba406a1d743bd01122ba594c96f86309a
AlexandrStreiD <125594207+AlexandrStreiD@users.noreply.github.com>
2023-03-31 13:23:36 +0300

    Delete Examp005 directory

--- Examp005/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-
-
-Console.WriteLine("Введите имя пользователя:");
-string name = Console.ReadLine();
-
-if(name.ToLower()=="маша"){ Console.WriteLine("Ура!!! Привет МАША!"); }
-else {Console.WriteLine("Привет " + name);}
-
