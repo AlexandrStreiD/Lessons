81fcb0bf1013c4faf4b6e280fd54a184788d913a
AlexandrStreiD <125594207+AlexandrStreiD@users.noreply.github.com>
2023-03-31 13:23:29 +0300

    Delete Examp004 directory

--- Examp004/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿double x =new Random().Next(1, 10); // случайное число от 1 - 9
-Console.WriteLine(x);
-double y =new Random().Next(1, 10);
-Console.WriteLine(y);
-double result = x / y;
-Console.WriteLine(result);
